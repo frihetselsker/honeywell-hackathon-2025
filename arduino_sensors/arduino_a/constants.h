64dfb3bd65e549a47857df207cecdc8ba086c519
Roi Bachynskyi <roybachynskyi@gmail.com>
2025-08-26 20:56:56 +0300

    feat: add constants for first board

---
+++ arduino_sensors/arduino_a/constants.h
@@ -0,0 +1,17 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Sensor pins
+inline constexpr int GAS_PIN   = A0;
+inline constexpr int NOISE_PIN = A1;
+inline constexpr int DHT_PIN   = 4;
+
+// Actuators
+inline constexpr int SERVO_PIN   = 5;
+inline constexpr int COOLER_PIN  = 6;
+inline constexpr int BUZZER_PIN  = 7;  // TBD
+
+// Serial communication
+inline constexpr long BAUD_RATE = 9600;
+
+#endif
