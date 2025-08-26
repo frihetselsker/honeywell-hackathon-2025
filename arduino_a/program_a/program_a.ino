7390b147280e91b07770233cba403f59424e3f65
Roi Bachynskyi <roybachynskyi@gmail.com>
2025-08-26 15:51:55 +0300

    motor

--- arduino_a/program_a/program_a.ino
+++ arduino_a/program_a/program_a.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 #include <dht11.h>
 #include <SoftwareSerial.h>
 
@@ -9,8 +11,15 @@ const int DHT11PIN = 4;
 float gasValue = 0;  // variable to store the value coming from the sensor
 float noiseValue = 0;
 
+int motorPin = 6; // Motor control pin (PWM)
+
 unsigned long lastSend = 0;
 const unsigned long period = 1000; // send once per second
+
+Servo myservo;  // create servo object to control a servo
+
+
+int pos = 0;    // variable to store the servo position
 
 
 
@@ -23,7 +32,10 @@ void setup() {
   mySerial.begin(9600);
   mySerial.setTimeout(50);     // faster reads if newline missing
   Serial.println("A: ready");
+  myservo.attach(5);
 
+  pinMode(motorPin, OUTPUT);
+  analogWrite(motorPin, 255); // Full speed
 
 }
 
@@ -59,5 +71,28 @@ void loop() {
   //Serial.println((float)DHT11.temperature, 2);
   // stop the program for <sensorValue> milliseconds:
   delay(200);
+
+  // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+
+  //   // in steps of 1 degree
+
+  //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
+
+  //   delay(15);                       // waits 15ms for the servo to reach the position
+
+  // }
+
+  // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+
+  //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
+
+  //   delay(15);                       // waits 15ms for the servo to reach the position
+
+  // }
+
+  analogWrite(motorPin, 255); // Full speed
+  // delay(2000);               // Run 2 seconds
+  // analogWrite(motorPin, 0);   // Stop
+  // delay(2000);          
 
 }
