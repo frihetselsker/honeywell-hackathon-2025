a143acc2c7609deca3bac7bcd0e8d8e9b8ad48bc
Roi Bachynskyi <roybachynskyi@gmail.com>
2025-08-26 13:14:47 +0300

    buzzer

--- arduino_b/lcd_display/lcd_display.ino
+++ arduino_b/lcd_display/lcd_display.ino
@@ -1,37 +1,16 @@
-#include <LiquidCrystal_I2C.h>
+/* Arduino tutorial - Buzzer / Piezo Speaker
+   More info and circuit: http://www.ardumotive.com/how-to-use-a-buzzer-en.html
+   Dev: Michalis Vasilakis // Date: 9/6/2015 // www.ardumotive.com */
 
-// |———————————————————————————————————————————————————————| 
-// |  made by Arduino_uno_guy 11/13/2019                   |
-// |   https://create.arduino.cc/projecthub/arduino_uno_guy|
-//  |———————————————————————————————————————————————————————|
+const int buzzer = 9; //buzzer to arduino pin 9
 
-
-#include  <Wire.h>
-
-//initialize the liquid crystal library
-//the first parameter is  the I2C address
-//the second parameter is how many rows are on your screen
-//the  third parameter is how many columns are on your screen
-LiquidCrystal_I2C lcd(0x27,  16, 2);
-
-void setup() {
-  
-  //initialize lcd screen
-  lcd.init();
-  // turn on the backlight
-  lcd.backlight();
+void setup(){
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
 }
-void loop() {
-  //wait  for a second
-  delay(1000);
-  // tell the screen to write on the top row
-  lcd.setCursor(0,0);
-  // tell the screen to write “hello, from” on the top  row
-  lcd.print("Hello, From");
-  // tell the screen to write on the bottom  row
-  lcd.setCursor(0,1);
-  // tell the screen to write “Arduino_uno_guy”  on the bottom row
-  // you can change whats in the quotes to be what you want  it to be!
-  lcd.print("Arduino_uno_guy");
-  
+
+void loop(){
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(1000);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  delay(1000);        // ...for 1sec
 }
