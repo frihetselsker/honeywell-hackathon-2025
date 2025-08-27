278ec77a903d2c66a4cea435288fffc3c536c2fa
Roi Bachynskyi <roybachynskyi@gmail.com>
2025-08-27 12:51:18 +0300

    rgb

--- arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller.ino
@@ -54,6 +54,7 @@ void setup() {
   initUI();
   initComms();
   initMisc();
+  setRGB(255, 0, 0);  // Set RGB to red
   Serial.begin(9600);
   Serial.println("B: ready");
   CoroutineScheduler::setup();
